70c39a0d8dcdc5bf38a597ccf5248ca740194a84
ThanhTinh <93694384+thanhtinhht2002@users.noreply.github.com>
2024-07-25 13:13:38 +0700

    .

---
+++ taxonomy-product-category.php
@@ -0,0 +1,119 @@
+
+    <main class="detail__product--main">
+        <div class="detail__product--container">
+            <div class="detail__product--navbar">
+                <div class="detail__product--navbar__categori">
+                    <img src="../assets/images/detail_product.png" alt="" srcset="">
+                    <div class="detail__product--navbar__searchbox">
+                        <input type="text" placeholder="Nhập từ khóa tìm kiếm . . .">
+                        <span class="detail__product--navbar__searchbox--btn">
+                            <ion-icon name="search-outline"></ion-icon>
+                        </span>
+                    </div>
+                    <div class="detail__product--navbar__func">
+                        <div class="detail__product--navbar__func--link">
+                            <div><ion-icon name="alarm-outline"></ion-icon></div>
+                            <div class="detail__product--navbar__func--link__name">
+                                Thông báo
+                            </div>
+                        </div>
+                        <div class="detail__product--navbar__func--link">
+                            <div><ion-icon name="cart-outline"></ion-icon></div>
+                            <div class="detail__product--navbar__func--link__name">
+                                Giỏ hàng
+                            </div>
+                        </div>
+                        <div class="detail__product--navbar__func--link">
+                            <div><ion-icon name="person-circle-outline"></ion-icon></div>
+                            <div class="detail__product--navbar__func--link__name">
+                                Name User
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="detail__product--box">
+                <div class="detail__product--media">
+                    <div class="detail__product--media__slide">
+                        <div class="detail__product--media__slide--imgs">
+                            <img src="../assets/images/book_detail.webp" alt="">
+                        </div>
+                        <div class="detail__product--media__slide--thumbnail">
+                            <div class="detail__media--list">
+                                <div class="detail__product--media__slide--items">
+                                    <img src="../assets/images/book_detail.webp" alt="">
+                                </div>
+                                <div class="detail__product--media__slide--items">
+                                    <img src="../assets/images/book_detail.webp" alt="">
+                                </div>
+                                <div class="detail__product--media__slide--items">
+                                    <img src="../assets/images/book_detail.webp" alt="">
+                                </div>
+                                <div class="detail__product--media__slide--items">
+                                    <img src="../assets/images/book_detail.webp" alt="">
+                                </div>
+                                <div class="detail__product--media__slide--items">
+                                    <img src="../assets/images/book_detail.webp" alt="">
+                                </div>
+                                <div class="detail__product--media__slide--items">
+                                    <img src="../assets/images/book_detail.webp" alt="">
+                                </div>
+                                <div class="detail__product--media__slide--items">
+                                    <img src="../assets/images/book_detail.webp" alt="">
+                                </div>
+
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <div class="detail__product--view__addbox">
+                        <button class="detail__product--addBtn detail__whitebtn">
+                            Thêm vào giỏ hàng
+                        </button>
+                        <button class="detail__product--addBtn detail__redbtn">
+                            Đặt trước
+                        </button>
+                    </div>
+                </div>
+                <div class="detail__product--detail">
+                    <div class="detail__product--inf">
+                        <div class="detail__product--title">
+                            Búp Sen Xanh (Tái Bản 2020)
+                        </div>
+                        <div class="detail__product--rating">
+                            4 * * * * *|Đã bán 3.7k
+                        </div>
+                        <div class="detail__product--price">
+                            <div class="detail__product--price__buy">61.200 đ</div>
+                            <div class="detail__product--price__sale"> 72.000 đ</div> <span
+                                class="detail__product--rate__sale">-15%</span>
+                        </div>
+                    </div>
+                    <div class="detail__product--describe">
+                        <div class="detail__product--describe__title">
+                            Mô tả sản phẩm
+                        </div>
+                        <div class="detail__product--describe__content">
+                            Có thể xếp “Búp Sen Xanh” vào nhóm tác phẩm văn học dành cho thiếu nhi và là tác phẩm nổi
+                            tiếng nhất viết về chủ tịch Hồ Chí Minh trong suốt thời thơ ấu cho đến khi rời Việt Nam sang
+                            Pháp.
+
+                            “Búp Sen Xanh” là nơi tiểu thuyết và lịch sử đã gặp nhau và hoạ nên một giai đoạn trong cuộc
+                            đời người Cha già của dân tộc Việt Nam. Nơi ấy, có quê nhà xứ Nghệ, có làng Sen, có khung
+                            dệt của mẹ, có lời dạy của cha, có những người bạn và những kỷ niệm ấu thơ. Nơi ấy có xứ Huế
+                            mà trong cuộc sống nghèo khổ có trăn trở tuổi trẻ, về con người, về vận mệnh dân tộc, có mất
+                            mát và đau thương...
+
+                            “Búp Sen Xanh” vượt ra ngoài những giới hạn của một tác phẩm thiếu nhi, có thể làm bất kỳ ai
+                            rung động đến rơi nước mắt trong đêm chia ly, khi người con từ biệt cha ra đi để tìm một con
+                            đường cho chính mình và cho dân tộc. Một phần cuộc đời, trọn vẹn lý tưởng và dấn thân... Búp
+                            Sen Xanh không chỉ là câu chuyện về lãnh tụ mà còn là câu chuyện để làm người.
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </main>
+   
--- templates/template-product-list.php
+++ templates/template-product-list.php
@@ -5,6 +5,12 @@
 ?>
 <?php get_header();?>
 <?php get_sidebar(); ?>
+ <?php wc_get_template('cart/cart.php'); ?>
+<?php wc_get_template('cart/cart-totals.php'); ?> 
+<div class="cart__container">
+<div class="cart__container--content">
 <?php wc_get_template('cart/cart.php'); ?>
+</div>
+<div class="cart__container--content">
 <?php wc_get_template('cart/cart-totals.php'); ?>
 <?php get_footer(); ?>
